24175eb4c4f506268e01629e44cfdcecc53835e8
Willy Tseng <willytseng@exosite.com>
2014-07-10 17:43:11 +0800

    Clean codes

--- index.js
+++ index.js
@@ -15,15 +15,6 @@ var extend = require('extend'),
     // progress = require('request-progress'),
     debug = require('debug')('widget-uploader');
 
-// basic auth
-var _opt = {
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36',
-        'Accept-Encoding': 'gzip'
-    },
-    strictSSL: false
-};
-
 function upload(opt) {
 
     var url = 'https://' + opt.host + '/api/portals/v1/widget-scripts/' + opt.widgetId;
@@ -34,24 +25,17 @@ function upload(opt) {
         headers: {
             'Content-Type': 'application/json',
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36',
-            'Accept-Encoding': 'gzip'
+            // 'Accept-Encoding': 'gzip'
         },
         auth: opt.auth,
         strictSSL: false,
         followRedirect: true
     };
 
-    // if (task.cookie && task.cookie[task.host]) {
-    // debug(task.cookie[task.host]);
-    // opt.jar.setCookie(task.cookie[task.host], 'https://' + task.host);
-    // }
-
     var input = reduce(joinResponse, '');
 
     var output = input.pipe(reduce(wrapAsJSON, '{"code":null}'))
-    // .pipe(zlib.createGzip())
-    .pipe(request.put(url, _opt, uploadResult)).on('error', console.error)
-        .pipe(zlib.createGunzip())
+        .pipe(request.put(url, _opt, uploadResult)).on('error', console.error)
         .pipe(reduce(joinResponse, '')) //.on('data', console.log)
 
     return es.duplex(input, output);
@@ -79,8 +63,6 @@ function uploadResult(err, response, body) {
 
 // a reduce call back
 function wrapAsJSON(prev, data) {
-    // console.log('wrapAsJSON');
-    // console.log(data);
     return data ? JSON.stringify({
         code: data.toString()
     }) : prev;
